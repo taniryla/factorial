11fa28eb80fc1cbefcffb9f91b1aca19443b6e6f
Ryland Taniguchi <ryland0000@gmail.com>
2022-05-10 05:26:04 +0200

    recursion

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-function factorial(num) {
-  let total = 1;
-  for (let i = num; i > 0; i--) {
-    total *= i;
-  }
-  return total;
-}
+// function factorial(num) {
+//   let total = 1;
+//   for (let i = num; i > 0; i--) {
+//     total *= i;
+//   }
+//   return total;
+// }
 
 factorial(4);
+
+function factorial(num) {
+  if (num === 1) return 1;
+  return num * factorial(num - 1);
+}
